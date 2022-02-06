e94d0d2c3ae200c7bf9825a909dae12fe08b8b1f
Raymond Cox <raymond@raymondjcox.com>
2022-02-06 13:58:59 -0500

    hook up notes fetch

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,26 @@
+import { Note } from "@prisma/client";
+import { useLoaderData } from "remix";
+import { PrismaClient } from "@prisma/client";
+import type { LoaderFunction } from "remix";
+
+const prisma = new PrismaClient();
+
+export let loader: LoaderFunction = async ({ params }) => {
+  return prisma.note.findMany();
+};
+
 export default function Index() {
+  const notes = useLoaderData<Note[]>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>notes</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {notes.map((note) => {
+          <li>
+            <a href="#">{note.title}</a>
+          </li>;
+        })}
       </ul>
     </div>
   );
